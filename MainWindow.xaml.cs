f17a249850a50f6b128f8a9ee57db5ffe1916871
dangvu2408 <hiend6887@gmail.com>
2024-09-21 04:05:27 +0700

    Remove .vs from being tracked

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -21,6 +21,22 @@ namespace wALLetApplication
             InitializeComponent();
         }
 
-        
+        private void ListBoxItem_Selected(object sender, SelectionChangedEventArgs e)
+        {
+            ListBoxItem selectedItem = (ListBoxItem)((ListBox)sender).SelectedItem;
+            if (selectedItem != null)
+            {
+                string selectedTag = selectedItem.Tag.ToString();
+                switch (selectedTag)
+                {
+                    case "Home":
+                        content.Content = new Views.Home(); // Trang chủ
+                        break;
+                    case "Query":
+                        content.Content = new Views.Query(); // Cài đặt
+                        break;
+                }
+            }
+        }
     }
 }
